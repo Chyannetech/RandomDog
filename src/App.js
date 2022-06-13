0edef538fe445ee530ba3cee0be980ca3dd0b671
Chyanne Robbins <chyannetech@gmail.com>
2022-06-12 23:05:35 -0400

    Generator Working Properly

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import DogImage from "./components/DogImage";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const API_URL = "https://dog.ceo/api/breeds/image/random";
+
+  // This is useState where we pass the initial state to this function and it returns a variable with the current state value
+  const [dog, setDog] = useState(null);
+
+  const getDog = async () => {
+    const response = await fetch(API_URL);
+
+    const data = await response.json();
+    setDog(data.message);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="title">Random Dog Generator</div>
+      <div className="subtitle">Check out these adorable doggies.</div>
+      <div className="submit">
+        <input type="button" value="Get Random Dog" onClick={getDog} />
+      </div>
+      <div className="dogImg">
+        <DogImage dog={dog} />
+      </div>
     </div>
   );
 }
---
+++ src/components/DogImage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DogImage = ({ dog }) => {
+  return <img src={dog} alt="" />;
+};
+
+export default DogImage;
